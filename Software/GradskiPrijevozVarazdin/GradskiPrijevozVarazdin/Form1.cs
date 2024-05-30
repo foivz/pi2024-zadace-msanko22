2b99bedbd41d114ecf821349334e059f4d8cf4a6
matej <msanko22@student.foi.hr>
2024-05-30 13:58:22 +0200

    Implementacija hijerarhije Implementacija hijerarhije te dizajn pocetne forme

--- Software/GradskiPrijevozVarazdin/GradskiPrijevozVarazdin/Form1.cs
+++ Software/GradskiPrijevozVarazdin/GradskiPrijevozVarazdin/Form1.cs
@@ -16,5 +16,20 @@ namespace GradskiPrijevozVarazdin
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Software/GradskiPrijevozVarazdin/GradskiPrijevozVarazdin/Models/Stanica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradskiPrijevozVarazdin.Models
+{
+    public class Stanica
+    {
+        public int BrStanice { get; set; }
+        public string NazivAdresaStanice { get; set; }
+        public int Brperonanastanici { get; set; }
+        public string Linija { get; set; }
+        public int Kapacitet { get; set; }
+
+        public override string ToString()
+        {
+            return NazivAdresaStanice + " " + Linija;
+        }
+
+    }
+}
