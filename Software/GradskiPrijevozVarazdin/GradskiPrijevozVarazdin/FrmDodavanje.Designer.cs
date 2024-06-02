9ba4fdb9e7a572560adf438a270fc81f6b2f933b
matej <msanko22@student.foi.hr>
2024-06-02 14:15:18 +0200

    Primjena dizajna u formi Dodavanje

--- Software/GradskiPrijevozVarazdin/GradskiPrijevozVarazdin/FrmDodavanje.Designer.cs
+++ Software/GradskiPrijevozVarazdin/GradskiPrijevozVarazdin/FrmDodavanje.Designer.cs
@@ -49,11 +49,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(105, 25);
+            this.label1.Location = new System.Drawing.Point(105, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(420, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Definiranje autobusne stanice";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label3
             // 
--- Software/GradskiPrijevozVarazdin/GradskiPrijevozVarazdin/FrmDodavanje.cs
+++ Software/GradskiPrijevozVarazdin/GradskiPrijevozVarazdin/FrmDodavanje.cs
@@ -43,5 +43,10 @@ namespace GradskiPrijevozVarazdin
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
